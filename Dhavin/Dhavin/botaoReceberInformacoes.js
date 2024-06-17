03f6e5bec1ac0e030d7443256a11144a2b0ca63f
Nid silva <80358224+Ivoneide01@users.noreply.github.com>
2024-06-17 13:16:37 -0300

    teste envio botao

--- Dhavin/Dhavin/botaoReceberInformacoes.js
+++ Dhavin/Dhavin/botaoReceberInformacoes.js
@@ -8,3 +8,33 @@ document.addEventListener("DOMContentLoaded", function() {
         alert("As informações foram enviadas!");
     });
 });
+
+// Suponha que você tenha um formulário com os campos name e email
+const form = document.getElementById("formularioContato");
+
+form.addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  // Captura dos valores dos campos do formulário
+  const name = form.elements["name"].value;
+  const email = form.elements["email"].value;
+
+  // Chama a função para enviar dados para o Firebase
+  enviarDadosFormulario(name, email)
+    .then(() => {
+      console.log('Dados do formulário de contato enviados com sucesso!');
+      // Exibe a mensagem de sucesso
+      const successMessage = document.getElementById('successMessage');
+      successMessage.style.display = 'block';
+      // Opcional: oculta a mensagem após 3 segundos
+      setTimeout(function() {
+        successMessage.style.display = 'none';
+      }, 3000);
+    })
+    .catch((error) => {
+      console.error('Erro ao enviar dados do formulário de contato:', error);
+    });
+
+  // Limpa o formulário após o envio
+  form.reset();
+});
